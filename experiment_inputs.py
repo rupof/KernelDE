a4bfe874318c41dc4e69ec243ee5adf567acef8f
rupof <rflorezablan@gmail.com>
2024-04-29 11:09:08 +0200

    np.array missing

--- experiment_inputs.py
+++ experiment_inputs.py
@@ -122,7 +122,7 @@ function_list_ho = [("harmonic_oscillator", [0, 1], np.linspace(0.0001, 1.5*3.14
 num_qubits_list = [8]
 num_layers_list = [3]
 quantum_bandwith = [1]
-gamma_classical_bandwidth_list = [1]
+gamma_classical_bandwidth_list = np.array([1])
 sigma_classical_bandwidth_list = 0.5*(1/gamma_classical_bandwidth_list)**2
 experiment_QNN_combination_ho = get_experiment_combination_list([function_list_ho, encoding_circuit_list, num_qubits_list, num_layers_list, sigma_classical_bandwidth_list, ["QNN"], executor_type_list, quantum_bandwith])
 
@@ -133,7 +133,7 @@ executor_type_list = ["pennylane"]
 num_qubits_list = [8]
 num_layers_list = [3]
 quantum_bandwith = [1]
-gamma_classical_bandwidth_list = [1]
+gamma_classical_bandwidth_list = np.array([1])
 sigma_classical_bandwidth_list = 0.5*(1/gamma_classical_bandwidth_list)**2
 experiment_QNN_combination_paper = get_experiment_combination_list([function_list, encoding_circuit_list, num_qubits_list, num_layers_list, sigma_classical_bandwidth_list, ["QNN"], executor_type_list, quantum_bandwith])
 
