Fixed  MSE calculation for index and implemented log ode.

DE_Library/diferential_equation_functionals.py
@@ -203,7 +203,34 @@ def loss_log_ode(f_alpha_tensor, x_span = None):
     k = 1
     return dfdx - np.exp(-f*k)*lamb 
 
+def grad_loss_log_ode(loss_values, x_span = None):
+    """
+    n = x_span.shape[0] number of points
+    m = x_span.shape[1] number of dimensions (typically m=1)
 
+    F[x, x_, x__] = F(x, x_, x__)
+
+    grad_F = (F(x, x_, x__)dx, F(x, x_, x__)dx_, F(x, x_, x__)dx__)
+
+    F = lamb * np.exp(f * k) - df/dx
+
+    grad_F = (-lamb*k, -1, 0)
+    """
+    lamb = 1
+    k = 1
+    x, f, dfdx, dfdxdx = get_differentials(loss_values, x_span)
+
+    
+    dfdp = loss_values["dfdp"] # shape (n, p)
+    n_param = dfdp.shape[1]
+
+    dFdf = lamb*k*np.exp(-f*k)
+
+    grad_envelope_list = np.zeros((3, x.shape[0], n_param)) # shape (3, n, p)
+    grad_envelope_list[0,:,:] = np.tile(dFdf, (n_param, 1)).T  
+    grad_envelope_list[1,:,:] =  1  # dF/dfdx
+    grad_envelope_list[2,:,:] =  0  # dF/dfdxdx
+    return grad_envelope_list
 
 def derivatives_loss_log_ode(f_alpha_tensor, x_span = None):
     """
@@ -321,7 +348,7 @@ mapping_of_derivatives_of_loss_functions = {
 
 mapping_of_grad_of_loss_functions = {
     "paper": grad_loss_paper,
-    "log_ode": derivatives_loss_log_ode,
+    "log_ode": grad_loss_log_ode,
     "polynomial_with_exp": derivatives_loss_polynomial_with_exp,
     "harmonic_oscillator": grad_loss_harmonic_oscillator,
     "paper_decay_QNN": grad_loss_paper_decay_QNN, 

DE_main.py
@@ -123,7 +123,7 @@ for idx, experiment in enumerate(experiment_list):
         numerical_solution = odeint(experiment["derivatives_of_loss"], f_initial, x_span[:])
         cache[solution_label] = numerical_solution
     
-    mse = np.mean((f_sol[0] - cache[solution_label][:,0]))**2
+    mse = np.mean((f_sol - cache[solution_label][:,0]))**2
 
     dict_to_save = {"f_sol": f_sol, 
                     "optimal_alpha": optimal_alpha, 

experiment_inputs.py
@@ -117,14 +117,15 @@ experiment_PQK_combination_ho = get_experiment_combination_list([function_list_h
 ###############
 
 
-encoding_circuit_list = ["YZ_CX_EncodingCircuit"]
-function_list_ho = [("harmonic_oscillator", [0, 1], np.linspace(0.0001, 1.5*3.14, 50))]
-num_qubits_list = [8]
-num_layers_list = [3]
+encoding_circuit_list = ["SimpleAnalyticalCircuit"]
+function_list_ho = [("harmonic_oscillator", [1, 0], np.linspace(0, 1*3.14, 20))]
+executor_type_list = ["pennylane"]    
+num_qubits_list = [1]
+num_layers_list = [1]
 quantum_bandwith = [1]
 gamma_classical_bandwidth_list = np.array([1])
 sigma_classical_bandwidth_list = 0.5*(1/gamma_classical_bandwidth_list)**2
-experiment_QNN_combination_ho = get_experiment_combination_list([function_list_ho, encoding_circuit_list, num_qubits_list, num_layers_list, sigma_classical_bandwidth_list, ["QNN"], executor_type_list, quantum_bandwith])
+experiment_QNN_combination_ho = get_experiment_combination_list([function_list_ho, encoding_circuit_list, num_qubits_list, num_layers_list, sigma_classical_bandwidth_list, ["QNN_pinned"], executor_type_list, quantum_bandwith])
 
 
 encoding_circuit_list = ["YZ_CX_EncodingCircuit"]
@@ -135,7 +136,7 @@ num_layers_list = [3]
 quantum_bandwith = [1]
 gamma_classical_bandwidth_list = np.array([1])
 sigma_classical_bandwidth_list = 0.5*(1/gamma_classical_bandwidth_list)**2
-experiment_QNN_combination_paper = get_experiment_combination_list([function_list, encoding_circuit_list, num_qubits_list, num_layers_list, sigma_classical_bandwidth_list, ["QNN"], executor_type_list, quantum_bandwith])
+experiment_QNN_combination_paper = get_experiment_combination_list([function_list, encoding_circuit_list, num_qubits_list, num_layers_list, sigma_classical_bandwidth_list, ["QNN_pinned"], executor_type_list, quantum_bandwith])
 
 encoding_circuit_list = ["ChebyshevTowerAndHEE"]
 function_list = [("paper", [1], np.linspace(0, 0.9, 20))]
@@ -162,35 +163,46 @@ experiment_QNN_decay_combination_paper_Chebyshev = get_experiment_combination_li
 encoding_circuit_list = ["ChebyshevTowerAndHEE"]
 function_list = [("paper_decay_QNN", [1], np.linspace(0, 0.9, 20))]
 executor_type_list = ["pennylane"]    
-num_qubits_list = [2]
+num_qubits_list = [6]
 num_layers_list = [5]
 quantum_bandwith = [1]
 gamma_classical_bandwidth_list = np.array([1])
 sigma_classical_bandwidth_list = 0.5*(1/gamma_classical_bandwidth_list)**2
-experiment_QNN_decay_combination_paper_floating_Chebyshev = get_experiment_combination_list([function_list, encoding_circuit_list, num_qubits_list, num_layers_list, sigma_classical_bandwidth_list, ["QNN_floating"], executor_type_list, quantum_bandwith])
-
+experiment_QNN_decay_combination_paper_floating_Chebyshev = get_experiment_combination_list([function_list, encoding_circuit_list, num_qubits_list, num_layers_list, sigma_classical_bandwidth_list, ["QNN_pinned"], executor_type_list, quantum_bandwith])
+#12
 
 encoding_circuit_list = ["ChebyshevTowerAndHEE"]
 function_list = [("paper_decay_QNN", [1], np.linspace(0, 0.9, 20))]
 executor_type_list = ["pennylane"]    
-num_qubits_list = [2]
-num_layers_list = [10]
+num_qubits_list = [6]
+num_layers_list = [5]
 quantum_bandwith = [1]
 gamma_classical_bandwidth_list = np.array([1])
 sigma_classical_bandwidth_list = 0.5*(1/gamma_classical_bandwidth_list)**2
 experiment_QNN_test = get_experiment_combination_list([function_list, encoding_circuit_list, num_qubits_list, num_layers_list, sigma_classical_bandwidth_list, ["QNN_floating"], executor_type_list, quantum_bandwith])
-
+#13
 
 encoding_circuit_list = ["ChebyshevTowerAndHEE"]
 function_list = [("simple_test_QNN", [1], np.linspace(0, 0.9, 20))]
 executor_type_list = ["pennylane"]    
-num_qubits_list = [2]
-num_layers_list = [7]
+num_qubits_list = [6]
+num_layers_list = [5]
 quantum_bandwith = [1]
 gamma_classical_bandwidth_list = np.array([1])
 sigma_classical_bandwidth_list = 0.5*(1/gamma_classical_bandwidth_list)**2
 experiment_QNN_test_with_RX = get_experiment_combination_list([function_list, encoding_circuit_list, num_qubits_list, num_layers_list, sigma_classical_bandwidth_list, ["QNN_pinned"], executor_type_list, quantum_bandwith])
+#14
 
+
+encoding_circuit_list = ["ChebyshevTowerAndHEE"]
+function_list = [("log_ode", [np.log(0.001)], np.linspace(0.001, 0.9, 20))]
+executor_type_list = ["pennylane"]    
+num_qubits_list = [6]
+num_layers_list = [5]
+quantum_bandwith = [1]
+gamma_classical_bandwidth_list = np.array([1])
+sigma_classical_bandwidth_list = 0.5*(1/gamma_classical_bandwidth_list)**2
+QNN_log_test = get_experiment_combination_list([function_list, encoding_circuit_list, num_qubits_list, num_layers_list, sigma_classical_bandwidth_list, ["QNN_pinned"], executor_type_list, quantum_bandwith])
 
 
 #QNN_floating
@@ -208,6 +220,7 @@ experiment_list_total = [experiment_first_combination, #0
                         experiment_QNN_decay_combination_paper_Chebyshev, #11
                         experiment_QNN_decay_combination_paper_floating_Chebyshev, #12
                         experiment_QNN_test,  #13
-                        experiment_QNN_test_with_RX #14
+                        experiment_QNN_test_with_RX, #14
+                        QNN_log_test, #15
                         ] 
 